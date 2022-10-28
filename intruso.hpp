de075cde71a014df082be56fc3eeb8f226f87109
feliphf <felip7696@gmail.com>
2022-10-28 15:12:17 -0300

    arquivos iniciais do vpl

---
+++ intruso.hpp
@@ -0,0 +1,15 @@
+#ifndef INTRUSO_HPP
+#define INTRUSO_HPP
+
+#include <iostream>
+#include <string>
+
+class Intruso{
+    /*Continue a implementação da classe Intruso*/
+
+    public:
+        void set_senha_vazada(std::string vazou);
+        std::string crack_senha();
+};
+
+#endif
