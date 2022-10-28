6967d175a06d6156737e62e4edc5c70044f2e480
yanfigueiras <yanfigueirasalves@gmail.com>
2022-10-28 18:02:30 -0300

    Função de setar senha funcional

--- intruso.cpp
+++ intruso.cpp
@@ -4,17 +4,25 @@
 
 
 void Intruso::set_senha_vazada (string vazou){
-
     // Remove os espaços da string
     vazou.erase(std::remove(vazou.begin(), vazou.end(), ' '), vazou.end());
 
-    char entrada[5][2];
-    int k = 0;
-    for (int i = 0; i < 5; i++){
-        for (int j = 0; j < 2; j++){
-            entrada[i][j] = vazou[j + k];
-        }
+    // Organiza as entradas em uma matriz
+    char entrada[5][2];    
+    for (int i = 0, k= 0; i < 5; i++, k+=2){      
+        entrada[i][0] = vazou[k];
+        entrada[i][1] = vazou[k+1];
     }
+
+    // Salva de acordo com sequência que o usuário digitou em uma string auxiliar
+    string senha;
+    for (int i = 10 ; i < vazou.size(); i++){
+        senha += entrada[abs(vazou[i] - 65)][0];
+        senha += entrada[abs(vazou[i] - 65)][1];
+    }
+
+    // Adiciona a sequência no vetor
+    _sequencia.push_back(senha);
 }
 
 string Intruso::crack_senha(){
